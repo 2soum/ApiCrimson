2d5a6ae93032c7fecb0d886021049fe217f8a6eb
2soum <2soum.pro@gmail.com>
2024-06-17 02:27:32 +0200

    test routes change

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+# contenu du fichier routes.py
+from flask import Blueprint, request, jsonify
 from app.database import get_db_reference
 
 main_bp = Blueprint('main', __name__)
@@ -17,7 +18,7 @@ def add_score():
     else:
         user_ref.update({'score': score})
 
-    return 'success', 201
+    return jsonify({'message': 'success'}), 201
 
 @main_bp.route('/get_score', methods=['GET'])
 def get_score():
@@ -26,9 +27,9 @@ def get_score():
     user = user_ref.get()
 
     if not user:
-        return 'No data found', 404
+        return jsonify({'message': 'No data found'}), 404
 
-    return str(user['score']), 200
+    return jsonify({'score': user['score']}), 200
 
 @main_bp.route('/update_score', methods=['PUT'])
 def update_score():
@@ -39,10 +40,10 @@ def update_score():
     user_ref = get_db_reference(f'users/{username}')
     user = user_ref.get()
     if not user:
-        return 'User not found', 404
+        return jsonify({'message': 'User not found'}), 404
 
     user_ref.update({'score': new_score})
-    return 'success', 200
+    return jsonify({'message': 'success'}), 200
 
 @main_bp.route('/delete_score', methods=['DELETE'])
 def delete_score():
@@ -52,18 +53,20 @@ def delete_score():
     user_ref = get_db_reference(f'users/{username}')
     user = user_ref.get()
     if not user:
-        return 'User not found', 404
+        return jsonify({'message': 'User not found'}), 404
 
     user_ref.delete()
-    return 'success', 200
+    return jsonify({'message': 'success'}), 200
 
 @main_bp.route('/top_scores', methods=['GET'])
 def top_scores():
     users_ref = get_db_reference('users')
     users = users_ref.get()
 
-    all_scores = [{'username': username, 'score': user_data['score']} for username, user_data in users.items()]
+    if not users:
+        return jsonify({'message': 'No data found'}), 404
 
-    all_scores.sort(key=lambda x: x['score'])
+    all_scores = [{'username': username, 'score': user_data['score']} for username, user_data in users.items()]
+    all_scores.sort(key=lambda x: x['score'], reverse=True)
     top_scores = all_scores[:10]
-    return str(top_scores), 200
+    return jsonify(top_scores), 200
