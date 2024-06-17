29c51157aca71a8efba4d3bfec57124033a54d9e
2soum <2soum.pro@gmail.com>
2024-06-17 03:00:30 +0200

    petit update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import main_bp
+from app.routes import main_bp  # Adjusted import
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
